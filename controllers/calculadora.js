9c83ae487bac3774b33ba9fd13b0aed36373af61
filipeguelber <filipe.guelber@gmail.com>
2015-05-26 09:08:02 -0300

    Update calculadora.js

--- controllers/calculadora.js
+++ controllers/calculadora.js
@@ -28,7 +28,7 @@ module.exports = function() {
 				},
 				
 				subtrair: function(numero1, numero2) {
-					return numero1 - numero2;
+					0;
 				},
 				
 				multiplicar: function(numero1, numero2) {
